9e27590690cf4492d03c5a8c52f573d069737dbc
paulasanchezms <paulasm01@gmail.com>
2025-05-24 18:02:58 +0200

    Validaciones

--- src/app/pages/register/register.page.ts
+++ src/app/pages/register/register.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService, AuthResponse } from '../../services/auth.service';
 import {
@@ -8,6 +8,8 @@ import {
   AbstractControl,
   ValidationErrors,
 } from '@angular/forms';
+import { TermsPatientPage } from '../terms-patient/terms-patient.page';
+import { ModalController } from '@ionic/angular';
 
 @Component({
   standalone: false,
@@ -19,50 +21,49 @@ export class RegisterPage implements OnInit {
   registerForm!: FormGroup;
   formSubmitted = false;
   message: string = '';
+  isMobile = false;
 
   constructor(
     private fb: FormBuilder,
     private router: Router,
-    private authService: AuthService
+    private authService: AuthService,
+    private modalController: ModalController
   ) {}
 
   ngOnInit(): void {
     this.registerForm = this.fb.group({
-      name: ['', [Validators.required, this.onlyLettersValidator]],
-      last_name: ['', [Validators.required, this.twoLastNamesValidator]],
-      email: [
-        '',
-        [Validators.required, Validators.email, this.customEmailValidator],
-      ],
-      password: [
-        '',
-        [Validators.required, Validators.minLength(6), this.passwordValidator],
-      ],
+      name: ['', [Validators.required, this.textValidator]],
+      last_name: ['', [Validators.required, this.textValidator]],
+      email: ['', [Validators.required, Validators.email, this.customEmailValidator]],
+      password: ['', [Validators.required, Validators.minLength(6), this.passwordValidator]],
       role: ['PATIENT'],
+      confirmPassword: ['', Validators.required],
+      termsAccepted: [false, Validators.requiredTrue]
+    }, {
+      validators: [this.passwordsMatchValidator]
     });
 
-    this.registerForm.get('name')!.valueChanges.subscribe((value) => {
-      if (typeof value === 'string') {
-        this.registerForm
-          .get('name')!
-          .setValue(value.trim(), { emitEvent: false });
-      }
+    ['name', 'last_name'].forEach(field => {
+      this.registerForm.get(field)?.valueChanges.subscribe(value => {
+        if (typeof value === 'string') {
+          this.registerForm.get(field)?.setValue(value.replace(/^\s+/, ''), { emitEvent: false });
+        }
+      });
     });
 
-
+    this.checkScreenSize();
   }
 
   onRegister(): void {
     this.formSubmitted = true;
     if (this.registerForm.invalid) return;
-  
+
     const raw = this.registerForm.value;
-  
+
     this.authService.register(raw).subscribe({
       next: (response: AuthResponse) => {
         localStorage.setItem('authToken', response.jwt);
         this.message = 'Registro exitoso. ¡Bienvenido!';
-  
         this.router.navigate(['/patient/home']);
       },
       error: (error) => {
@@ -83,20 +84,10 @@ export class RegisterPage implements OnInit {
     return null;
   }
 
-  onlyLettersValidator(control: AbstractControl): ValidationErrors | null {
-    const onlyLettersRegex = /^[a-zA-ZÀ-ÿ\s]+$/;
-    if (control.value && !onlyLettersRegex.test(control.value.trim())) {
-      return { onlyLetters: true };
-    }
-    return null;
-  }
-
-  twoLastNamesValidator(control: AbstractControl): ValidationErrors | null {
-    const value = control.value?.trim();
-    if (!value) return null;
-  
-    const regex = /^[a-zA-ZáéíóúÁÉÍÓÚñÑ]+(?: [a-zA-ZáéíóúÁÉÍÓÚñÑ]+)*$/;
-    return regex.test(value) ? null : { invalidLastName: true };
+  textValidator(control: AbstractControl): ValidationErrors | null {
+    const trimmed = (control.value || '').trim();
+    const regex = /^[A-Za-zÁÉÍÓÚáéíóúÑñ]+(?: [A-Za-zÁÉÍÓÚáéíóúÑñ]+)*$/;
+    return trimmed && !regex.test(trimmed) ? { invalidText: true } : null;
   }
 
   customEmailValidator(control: AbstractControl): ValidationErrors | null {
@@ -107,6 +98,24 @@ export class RegisterPage implements OnInit {
     const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
     if (!emailRegex.test(value)) return { invalidEmail: 'Formato inválido.' };
     return null;
+  }
+
+  async openTermsModal() {
+    const modal = await this.modalController.create({
+      component: TermsPatientPage,
+      breakpoints: [0.3, 0.5, 0.9],
+      initialBreakpoint: 0.8
+    });
+    await modal.present();
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.checkScreenSize();
+  }
+
+  checkScreenSize() {
+    this.isMobile = window.innerWidth <= 768;
   }
 
   get name() {
@@ -120,5 +129,21 @@ export class RegisterPage implements OnInit {
   }
   get password() {
     return this.registerForm.get('password');
+  }
+  get termsAccepted() {
+    return this.registerForm.get('termsAccepted');
+  }
+
+  get confirmPassword() {
+    return this.registerForm.get('confirmPassword');
+  }
+
+  passwordsMatchValidator(form: AbstractControl): ValidationErrors | null {
+    const password = form.get('password')?.value;
+    const confirmPassword = form.get('confirmPassword')?.value;
+    if (password !== confirmPassword) {
+      return { passwordsMismatch: true };
+    }
+    return null;
   }
 }
